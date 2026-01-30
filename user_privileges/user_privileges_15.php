19351ca8e7ffe922af11bb761db6c635e1a27c45
QHUY <quochuyhuy1112@gmail.com>
2026-01-30 16:07:44 +0700

    user_privileges: sharing_privileges_15, user_privileges_15

---
+++ user_privileges/user_privileges_15.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=false;
+
+$current_user_roles='H1';
+
+$current_user_parent_role_seq='H1';
+
+$current_user_profiles=array();
+
+$profileGlobalPermission=array();
+
+$profileTabsPermission=array('3'=>0,);
+
+$profileActionPermission=array();
+
+$current_user_groups=array();
+
+$subordinate_roles=array('H2','H3','H4','H5',);
+
+$parent_roles=array();
+
+$subordinate_roles_users=array('H2'=>array(1,5,8,12,14,),'H3'=>array(7,13,),'H4'=>array(6,),'H5'=>array(11,),);
+
+$user_info=array('user_name'=>'a4','is_admin'=>'off','user_password'=>'$2y$10$eDA650e1OME2riefRZdEuuMTCFKpBqKozLJUobCVnE8jcYto5iDie','confirm_password'=>'$2y$10$vGSXk11fCywNyG8cVczv1ebop/ey0AnhYu2OJDXWijNNb7IqU6NTq','first_name'=>'lilpin','last_name'=>'Go','roleid'=>'H1','email1'=>'a4@abc.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'','hour_format'=>'12','end_hour'=>'','start_hour'=>'09:00','is_owner'=>'','title'=>'razer','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'V38FF6SJFbN8EAdk','time_zone'=>'Atlantic/South_Georgia','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'en_us','reminder_interval'=>'','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'','defaulteventstatus'=>'','defaultactivitytype'=>'','hidecompletedevents'=>'0','defaultcalendarview'=>'','defaultlandingpage'=>'','userlabel'=>'lilpin Go','currency_name'=>'USA, Dollars','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.00000','record_id'=>'','id'=>'15');
+?>
